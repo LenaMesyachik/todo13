70c825a0eda1a0bb4e7c751b3985f791324f1f54
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-02-01 23:42:19 +0300

    some refactoring

--- src/api/todolists-api.tsx
+++ src/api/todolists-api.tsx
@@ -1,35 +1,26 @@
 import axios from "axios";
 
+
+const instance = axios.create({
+    baseURL:'/1.1',
+    withCredentials: true,
+    headers: {
+        'API-KEY': '55360c88-929c-4769-8aa8-2c2f9e14b649'
+    }
+})
 export const todolistApi = {
     getTodolist() {
-        let x = axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', {withCredentials: true})
-        return x
+        return instance.get('/1.1/todo-lists')
     },
     createTodolist() {
         const title = 'hhhhh'
-        let y = axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', {title: title}, {
-            withCredentials: true, headers: {
-                'API-KEY': '55360c88-929c-4769-8aa8-2c2f9e14b649'
-            }
-        })
-        return y
+       return  instance.post('/1.1/todo-lists', {title: title})
     },
     deleteTodolist(todolistId:string) {
-        let promise = axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`, {
-            withCredentials: true,
-            headers: {
-                'API-KEY': '55360c88-929c-4769-8aa8-2c2f9e14b649'
-            }
-        })
-        return promise
+         return instance.delete(`/todo-lists/${todolistId}`)
     },
+
     updateTodolist(todolistId:string,title:string) {
-        let pr = axios.put(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`, {title: title}, {
-            withCredentials: true,
-            headers: {
-                'API-KEY': '55360c88-929c-4769-8aa8-2c2f9e14b649'
-            }
-        })
-        return pr
+       return instance.put(`/1.1/todo-lists/${todolistId}`, {title: title})
     }
 }
