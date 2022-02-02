da31ba7bc459afb3aebc7da8c3b5572a3fc3eaa9
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-02-02 21:56:56 +0300

    create type ResponseType <T>

--- src/api/todolists-api.tsx
+++ src/api/todolists-api.tsx
@@ -8,21 +8,30 @@ const instance = axios.create({
         'API-KEY': '55360c88-929c-4769-8aa8-2c2f9e14b649'
     }
 })
+
+
 export const todolistApi = {
     getTodolist() {
         return instance.get<Array<TodoType>>('/todo-lists')
     },
     createTodolist() {
         const title = 'hhhhh'
-       return  instance.post<Array<CreateTodoType>>('/todo-lists', {title: title})
+       return  instance.post<ResponseType<{item:TodoType}>>('/todo-lists', {title: title})
     },
     deleteTodolist(todolistId:string) {
-         return instance.delete(`/todo-lists/${todolistId}`)
+         return instance.delete<ResponseType<{}>>(`/todo-lists/${todolistId}`)
     },
 
     updateTodolist(todolistId:string,title:string) {
-       return instance.put(`/todo-lists/${todolistId}`, {title: title})
+       return instance.put<ResponseType<{}>>(`/todo-lists/${todolistId}`, {title: title})
     }
+}
+
+type ResponseType <T> = {
+    fieldsErrors: string[],
+    messages:string[],
+    resultCode: number,
+    data:T
 }
 type TodoType = {
     id: string,
@@ -32,14 +41,15 @@ type TodoType = {
 }
 type CreateTodoType = {
     data:{
-        item:   {
-            id: string,
-            title: string,
-            addedDate:string,
-            order: number
-        }
+        item:   TodoType
     },
-    fieldsErrors:[],
-    messages:[],
-    resultCode: 0
+    fieldsErrors: string[],
+    messages:string[],
+    resultCode: number
    }
+type DeleteUpdateTodoType = {
+    data:{ },
+    fieldsErrors:string[],
+    messages:string[],
+    resultCode: number
+}
